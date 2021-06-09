4d8e01dbf4829b8b74cfbc0191fa69d6120d1e38
Mikaela Alfvén <mikalf@jetshop.se>
2021-06-09 20:05:18 +0200

    onsdag 20:04 icke fungerande query

--- browseplaylist.php
+++ browseplaylist.php
@@ -2,12 +2,29 @@
 <?php include 'header.php'?>
 
 <?php $_SESSION['historySearch']=""; ?>
+<?php 
+
+$userID = $_SESSION['userID'];
+$playlistID = 32; //explode('?', $_SERVER['REQUEST_URI']);
+//$playlistID = end($playlistID);
+
+
+  $getPlaylistQuery = "SELECT * FROM PlaylistSongs JOIN Songs ON Songs.SongID = PlaylistSongs.SongID 
+  JOIN Playlists ON Playlists.PlaylistID = UserPlaylist.PlaylistID WHERE Playlists.PlaylistID = $playlistID";
+  $getPlaylist = mysqli_query($db, $getPlaylistQuery);
+  $rows = mysqli_fetch_assoc($getPlaylist);
+  $songID = $rows['SongID'];
+  $username = $rows ['Username'];
+  echo $username;
+
+
+?>
 
 <!DOCTYPE html>
 <body class ="backgroundBlue"> 
 
 
-<div class="breadcrumb-box">
+<!--<div class="breadcrumb-box">
         <ul class="breadcrumbs">
             <li class="breadcrumb-item">
                 <a href="startpage.php" class="crumb">home</a>
@@ -16,65 +33,44 @@
                 <a href="browseplaylist.php" class="crumb">php variabeln hallå</a>
             </li>
         </ul>
-    </div>
+    </div>-->
 
-<h2 class="specificPlaylistTitle">php variabeln hallå</h2>
-<h3 class="specificPlaylistUsername">by php variabel user</h3>
+<h2 class="specificPlaylistTitle">php variable playlist title</h2>
+<h3 class="specificPlaylistUsername">by<?php echo $username ?> </h3>
+<?php showResults();?>
 
-    <div class="tablePlaylist">
-   <table> 
-    <tr>
-    <th>Artist</th>
-    <th>Title</th>
-    <th>Genre</th>
-    <th>Duration</th>
-    <th> ♡ </th>
-  </tr>
-  <tr>
-    <td>Queen</td>
-    <td>Bohemian Rhapsody</td>
-    <td>Rock</td>
-    <td>05:55</td>
-    <td></td>
-  </tr>
-  <tr>
-    <td>Dire Straits</td>
-    <td>Sultans of Swing</td>
-    <td>Blues Rock</td>
-    <td>05:48</td>
-    <td></td>
-  </tr>
-  <tr>
-    <td>Muse</td>
-    <td>Uprising</td>
-    <td>Electronic Rock</td>
-    <td>05:04</td>
-    <td></td>
-  </tr>
-  <tr>
-    <td>The Killers</td>
-    <td>Mr. Brightside</td>
-    <td>Alternative</td>
-    <td>03:42</td>
-    <td></td>
-  </tr>
-  <tr>
-    <td>Bob Dylan</td>
-    <td>Lay, Lady, Lay</td>
-    <td>Jazz</td>
-    <td>03:17</td>
-    <td></td>
-  </tr>
-  <tr>
-    <td>Nicky Jam; Enrique Iglesias</td>
-    <td>El Perdón</td>
-    <td>Reggaeton</td>
-    <td>03:27</td>
-    <td></td>
-  </tr>
-</table>
-    </div>
+<?php
+function showResults() {
 
+  //for ($i = 0; $i < 150; $i++){
+
+    
+  //}
+        //echo "<div class ='table'>";
+        //echo "<table id='fjomp'>";
+        //echo "<tr class ='createTableRow'>";
+        //echo "<th class ='createTableTitles'></th>";
+        //echo "<th class ='createTableTitles'>Artist</th>";
+        //echo "<th class ='createTableTitles'>Title</th>";
+        //echo "<th class ='createTableTitles'>Album</th>";
+        //echo "<th class ='createTableTitles'>Duration</th>";
+        //echo "</tr>";
+        //echo "<td><form id='formSearch' method='POST' enctype='text'><input class='addTrack' type='submit' value ='love' name='addTrack_$i' id='addTrack_$i'></input></form></td>";
+        //echo "<td>" . $data[$i]['artist']['name'] . "</td>";
+        //echo "<td>" . $data[$i]['title'] . "</td>";
+        //echo "<td>" . $data[$i]['album']['title'] . "</td>";
+        //echo "<td>" . gmdate("i:s", $data[$i]['duration']) . "</td>";
+        //echo "</tr>";
+      }
+    
+      //echo "</table>";
+      //echo "</div>";
+      //echo "</div>";
+
+  
+
+  
+  ?>
 
 
 <?php include 'footer.php' ?>
